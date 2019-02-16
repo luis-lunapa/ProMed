cd5b86151ec61f86f6bccafe53582935bed07e8c
luisLuna24 <luis.lunapa@outlook.com>
2019-02-15 23:03:50 -0600

    Primer commit

---
+++ ProMed/LoginViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LoginViewController.swift
+//  ProMed
+//
+//  Created by Luis Luna on 2/15/19.
+//  Copyright © 2019 DeepTech. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let tap = UITapGestureRecognizer.init(target: self, action: #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(tap)
+        self.userNameTextField.customPlaceHolder(color: .white)
+        self.passwordTextField.customPlaceHolder(color: .white)
+        self.loginButton.layer.cornerRadius = 5
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        self.userNameTextField.setBottomLine(borderColor: .lightGray)
+        self.passwordTextField.setBottomLine(borderColor: .lightGray)
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ProMed/Utilidades.swift
@@ -0,0 +1,61 @@
+//
+//  Utilidades.swift
+//  ProMed
+//
+//  Created by Luis Luna on 2/15/19.
+//  Copyright © 2019 DeepTech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Utilidades {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+extension UITextField {
+    
+    func setBottomLine(borderColor: UIColor) {
+        
+        self.borderStyle = UITextField.BorderStyle.none
+        self.backgroundColor = UIColor.clear
+        
+        let borderLine = UIView()
+        let height = 1.0
+        borderLine.frame = CGRect(x: 0, y: Double(self.frame.height) - height, width: Double(self.frame.width), height: height)
+        
+        borderLine.backgroundColor = borderColor
+        self.addSubview(borderLine)
+    }
+    
+    func setBottomLineWithBackground(borderColor: UIColor, background: UIColor) {
+        
+        self.backgroundColor = background
+        self.layer.cornerRadius = 10
+        
+        let borderLine = UIView()
+        let height = 1.0
+        borderLine.frame = CGRect(x: 0, y: Double(self.frame.height) - height, width: Double(self.frame.width), height: height)
+        
+        borderLine.backgroundColor = borderColor
+        self.addSubview(borderLine)
+    }
+    
+    func customPlaceHolder(color: UIColor){
+        var placeholderText = ""
+        if self.placeholder != nil{
+            placeholderText = self.placeholder!
+        }
+        self.attributedPlaceholder = NSAttributedString(string: placeholderText, attributes: [NSAttributedString.Key.foregroundColor : color])
+    }
+    
+}
